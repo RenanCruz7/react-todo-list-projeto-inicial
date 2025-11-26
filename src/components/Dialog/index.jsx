c3e2a3b34bea93334dbfccab95bae54387996dd6
RenanCruz7 <renancruz199@gmail.com>
2025-11-26 18:42:04 -0300

    adiciona componente Dialog com estilo de fundo

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,11 +1,23 @@
+import { useRef } from 'react';
+import './dialog.style.css';
 export function Dialog() {
+    const dialogRef = useRef(null);
+
+    const openDialog = () => {
+        dialogRef.current.showModal()
+    }
+
+    const closeDialog = () => {
+        dialogRef.current.close()
+    }
+
     return (
         <>
-        <dialog>
-            <button autofocus>Close</button>
+        <dialog ref={dialogRef}>
+            <button autoFocus onClick={closeDialog}>Close</button>
             <p>This modal dialog has a groovy backdrop!</p>
         </dialog>
-        <button>Show the dialog</button>
+        <button onClick={openDialog}>Show the dialog</button>
         </>
     )
 }
