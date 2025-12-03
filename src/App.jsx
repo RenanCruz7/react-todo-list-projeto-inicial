b90517be7869d84a5395acc33d25883b52adfb06
RenanCruz7 <renancruz199@gmail.com>
2025-12-03 18:58:16 -0300

    refatora lógica de gerenciamento de tarefas e adiciona funcionalidade de conclusão

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,51 @@ import { ToDoItem } from "./components/ToDoItem"
 import { ToDoList } from "./components/ToDoList"
 import { ToDoForm } from "./components/ToDoForm"
 
-const todos = [
+// const todos = [
+//   {
+//     id: 1,
+//     description: "JSX e componentes",
+//     completed: false,
+//     createdAt: "2022-10-31"
+//   },
+//   {
+//     id: 2,
+//     description: "Props, state e hooks",
+//     completed: false,
+//     createdAt: "2022-10-31"
+//   },
+//   {
+//     id: 3,
+//     description: "Ciclo de vida dos componentes",
+//     completed: false,
+//     createdAt: "2022-10-31"
+//   },
+//   {
+//     id: 4,
+//     description: "Testes unitários com Jest",
+//     completed: false,
+//     createdAt: "2022-10-31"
+//   }
+// ]
+// const completed = [
+//   {
+//     id: 5,
+//     description: "Controle de inputs e formulários controlados",
+//     completed: true,
+//     createdAt: "2022-10-31"
+//   },
+//   {
+//     id: 6,
+//     description: "Rotas dinâmicas",
+//     completed: true,
+//     createdAt: "2022-10-31"
+//   }
+// ]
+
+function App() {
+
+  const [showDialog, setShowDialog] = useState(false)
+  const [todos, setTodos] = useState([
   {
     id: 1,
     description: "JSX e componentes",
@@ -20,52 +64,44 @@ const todos = [
     createdAt: "2022-10-31"
   },
   {
-    id: 2,
-    description: "Props, state e hooks",
-    completed: false,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 3,
-    description: "Ciclo de vida dos componentes",
-    completed: false,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 4,
-    description: "Testes unitários com Jest",
-    completed: false,
-    createdAt: "2022-10-31"
-  }
-]
-const completed = [
-  {
     id: 5,
     description: "Controle de inputs e formulários controlados",
     completed: true,
     createdAt: "2022-10-31"
-  },
-  {
-    id: 6,
-    description: "Rotas dinâmicas",
-    completed: true,
-    createdAt: "2022-10-31"
   }
-]
-
-function App() {
-
-  const [showDialog, setShowDialog] = useState(false)
+  ])
 
   const toggleDialog = () => {
     setShowDialog(!showDialog)
   }
 
-  const addTodo = () =>{
-    console.log("Adicionar todo")
+  const addTodo = ( formData ) =>{
+    const description = formData.get("description")
+    setTodos(prevState => {
+      const newTodo = {
+        id: prevState.length + 1,
+        description: description,
+        completed: false,
+        createdAt: new Date().toISOString()
+      }
+      return [...prevState, newTodo]
+    })
     toggleDialog()
   }
 
+  const toggleTodoCompleted = (todo) => {
+    setTodos(prevState =>{
+      return prevState.map(t => {
+        if(t.id === todo.id){
+          return {
+            ...t,
+            completed: !t.completed
+          }
+        }
+        return t
+      })
+    })
+  }
 
   return (
     <main>
@@ -78,14 +114,14 @@ function App() {
         <ChecklistsWrapper>
           <SubHeading>Para estudar</SubHeading>
           <ToDoList>
-            {todos.map(function (t) {
-              return <ToDoItem key={t.id} item={t} />
+            {todos.filter(t => !t.completed).map(function (t) {
+              return <ToDoItem key={t.id} item={t} onToggleCompleted={toggleTodoCompleted} />
             })}
           </ToDoList>
           <SubHeading>Concluído</SubHeading>
           <ToDoList>
-            {completed.map(function (t) {
-              return <ToDoItem key={t.id} item={t} />
+            {todos.filter(t => t.completed).map(function (t) {
+              return <ToDoItem key={t.id} item={t} onToggleCompleted={toggleTodoCompleted}   />
             })}
           </ToDoList>
           <Footer>
